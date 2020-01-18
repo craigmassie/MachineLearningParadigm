6b8f379dea66247b517af84e8e1a80a02d150759
Craig Massie <craigsmassie@gmail.com>
2020-01-18 19:13:01 +0000

    Minor refactor of detect_models

--- apis/detect_models/detect_models.py
+++ apis/detect_models/detect_models.py
@@ -4,6 +4,7 @@ import json
 
 app = Flask(__name__)
 api = Api(app)
+ARCHITECTURE_FILE_LOCATION = "available_models.json"
 
 @app.route('/detectModels', methods=['GET'])
 def get():
@@ -12,15 +13,22 @@ def get():
     return model_filetype_match(filetype)
 
 def model_filetype_match(filetype):
+    '''
+    Given the filetype provided, determines which type of architectures are suitable.
+    Subsequently, based on the architcture, will return pre-existing models of such, and their location.
+    '''
     architectures = {}
     models = []
-    with open('available_models.json') as json_file:
-        data = json.load(json_file)
-        for key in data["mappings"].keys():
-            architectures[key] = data["mappings"][key]
+    with open(ARCHITECTURE_FILE_LOCATION) as json_file:
+        available_models = json.load(json_file)
+        #architectures takes on the form: {File Type Class: [List of architectures]}
+        architectures = available_models["mappings"]
         if filetype in architectures.keys():
             for architecture in architectures[filetype]:
-                models.append(data["architectures"][architecture])
+                models.append(available_models["architectures"][architecture])
+        if models == []:
+            app.logger.info(f"There exist no models with the filetype: {filetype}")
     return {"models": models}
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
